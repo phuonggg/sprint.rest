3b2b82828391dbb9ac98d8066432f2a9a40dbe18
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-04-20 17:58:41 +0900

    3 endpoints passed
    Co-authored-by: phuonggg <phuonggg@users.noreply.github.com>
    Co-authored-by: maegatro <maegatro@users.noreply.github.com>

--- src/server.js
+++ src/server.js
@@ -1,9 +1,50 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+const express = require("express");
 const pokeData = require("./data");
 
 const setupServer = () => {
   /**
    * Create, set up and return your express server, split things into separate files if it becomes too long!
    */
+
+  const app = express();
+  app.use(express.json()); //MIDDLEWARE
+
+  // console.log('pokeData :', pokeData.pokemon)
+  //1
+  app.get("/api/pokemon/", (request, response) => {
+    const limit = request.query.limit;
+    // console.log(limit)
+    // response.send(pokeData.pokemon)
+    response.send(pokeData.pokemon.slice(0, limit));
+    // console.log(pokeData.pokemon.slice(0, limit))
+    // console.log(pokeData.pokemon.length , 'YOOOOOOOO')
+  });
+
+  app.post("/api/pokemon/", (request, response) => {
+    const newPoke = {
+      id: "152",
+      name: "PhuShunCarlosaur",
+    };
+    pokeData.pokemon.push(newPoke);
+    response.send(pokeData.pokemon);
+    // console.log(pokeData.pokemon.length, 'SUPPPPPPP')
+    //response.status(201).end();
+  });
+
+  //GET /api/pokemon/:id
+  // It should return the Pokemon with the given id. Example: GET /api/pokemon/042 should return the data for Golbat
+  // Leading zeroes should not be necessary, so GET /api/pokemon/42 would also return Golbat
+  app.get("/api/pokemon/:id", (request, response) => {
+    const id = request.params.id;
+    console.log("id :", id);
+    const result = pokeData.pokemon.filter((pokemon) => pokemon.id === id);
+    console.log("resultttttttttttttttttttttttttttttttttttt :", result[0]);
+    response.send(result[0]);
+  });
+
+  return app;
 };
 
 module.exports = { setupServer };
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -1,8 +1,10 @@
+/* eslint-disable prettier/prettier */
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const { setupServer } = require("../src/server");
 
+chai.should();
 /*
  * This sprint you will have to create all tests yourself, TDD style.
  * For this you will want to get familiar with chai-http https://www.chaijs.com/plugins/chai-http/
@@ -14,4 +16,203 @@ describe("Pokemon API Server", () => {
   beforeEach(() => {
     request = chai.request(server);
   });
+
+  describe("GET /api/pokemon - returning full list pokemon", () => {
+    it("should return json for full list pokemon", async () => {
+      const res = await request.get("/api/pokemon").query({ limit: 3 });
+
+      res.should.be.json;
+      console.log("res.text.length XXXXXXX:", JSON.parse(res.text).length);
+
+      JSON.parse(res.text).length.should.equal(3);
+    });
+
+    describe("POST /api/pokemon", () => {
+      it("It should add a Pokemon. ", async () => {
+        // const newPoke = {
+        //     "id": "152",
+        //     "name": "PhuShunCarlosaur"
+        //   }
+        const res = await request.post("/api/pokemon");
+        // .send({ newPoke });
+        // res.should.have.status(201);
+        res.should.be.json;
+        JSON.parse(res.text).length.should.equal(152);
+      });
+    });
+
+    //3
+    describe.only("GET /api/pokemon - It should return the Pokemon with the given id", () => {
+      it("It should return the Pokemon with the given id", async () => {
+        const res = await request.get("/api/pokemon/001");
+        // console.log("BBBBBBBB", (JSON.parse(res.text.name))
+
+        //res.should.be.json;
+
+        JSON.parse(res.text).name.should.equal("Bulbasaur");
+        // console.log('MEOOOOOOO', res.text)
+        // res.name.should.equal("Bulbasaur");
+      });
+    });
+  });
 });
+
+//           it("Returns status 401 if token is invalid", async () => {
+//   const res = await request
+//     .post("/secret/message")
+//     .query({ token: 3 })
+//     .send({ shout: "marco" });
+//   res.should.have.status(401);
+// });
+
+// it("Returns status 403 if token is valid, but the secret part is missing", async () => {
+//   const res = await request.post("/secret/message").query({ token: 6 });
+//   res.should.have.status(403);
+// });
+
+// it("Returns status 401 if anything is wrong", async () => {
+//   const res = await request.post("/secret/message");
+//   res.should.have.status(401);
+
+// describe("express basics", () => {
+//   describe("GET /teapot - modifying status", () => {
+//     it("should return status 418", async () => {
+//       const res = await request.get("/teapot");
+//       res.should.have.status(418);
+//     });
+//   });
+
+// describe("GET /hello - returning text", () => {
+//   it("should return the text/html 'world'", async () => {
+//     const res = await request.get("/hello");
+//     res.should.be.html;
+//     res.text.should.equal("world");
+//   });
+// });
+
+// describe("GET /hellojson - returning json", () => {
+//   it("should return the JSON for { hello: 'world' }", async () => {
+//     const res = await request.get("/hellojson");
+//     res.should.be.json;
+//     JSON.parse(res.text).should.deep.equal({ hello: "world" });
+//   });
+// });
+
+// describe("GET /greet - dealing with query parameters", () => {
+//   it("should greet the name given in query parameter 'name' with 'Hello name!'", async () => {
+//     const res = await request.get("/greet").query({ name: "Mia" });
+//     /* GET /greet?name=Mia */
+//     res.should.be.html;
+//     res.text.should.equal("Hello Mia!");
+//   });
+// });
+
+// xdescribe("GET /:a/plus/:b - dealing with params", () => {
+//   it("should return a JSON object with a result field", async () => {
+//     const res = await request.get("/1/plus/1");
+//     res.should.be.json;
+//     JSON.parse(res.text).result.should.not.be.undefined;
+//   });
+
+//   it("adds 2 + 3", async () => {
+//     const res = await request.get("/2/plus/3");
+//     res.should.be.json;
+//     JSON.parse(res.text).result.should.equal(5);
+//   });
+
+//   it("adds 3 + 2", async () => {
+//     const res = await request.get("/3/plus/2");
+//     res.should.be.json;
+//     JSON.parse(res.text).result.should.equal(5);
+//   });
+// });
+// });
+
+// xdescribe("handling bodies", () => {
+// xdescribe("/echo endpoint", () => {
+//   it("POST /echo returns body content", async () => {
+//     const expected = {
+//       foo: "bar",
+//       honey: ["I", "shrank", "the", "kids"],
+//       loopy: {
+//         loop: {
+//           deeply: {
+//             nested: [1, "123", [{ lol: "lol" }, null, null, 5]],
+//           },
+//         },
+//       },
+//     };
+//     const res = await request.post("/echo").send(expected);
+//     res.should.be.json;
+//     JSON.parse(res.text).should.deep.equal(expected);
+//   });
+
+//   it("OPTIONS /echo flips keys and values of simple objects", async () => {
+//     const payload = {
+//       what: "is love?",
+//       baby: "don't hurt me",
+//       "don't hurt me": "no more",
+//       "The Answer": 42,
+//     };
+
+//     const expected = {
+//       "is love?": "what",
+//       "don't hurt me": "baby",
+//       "no more": "don't hurt me",
+//       42: "The Answer",
+//     };
+
+//     const res = await request.options("/echo").send(payload);
+//     res.should.be.json;
+//     JSON.parse(res.text).should.deep.equal(expected);
+//   });
+// });
+// });
+
+// xdescribe("Adding middleware", () => {
+// xdescribe("/secret Endpoint", () => {
+//   it("Returns status 401 when called normally", async () => {
+//     const res = await request.get("/secret");
+//     res.should.have.status(401);
+//   });
+
+//   it("Returns status 200 when given a token query parameter that contains an even number", async () => {
+//     const res = await request.get("/secret").query({ token: 2018 });
+//     res.should.have.status(200);
+//   });
+
+//   it("Returns status 401 when given a token query parameter that is not an even number", async () => {
+//     const res = await request.get("/secret").query({ token: 45 });
+//     res.should.have.status(401);
+//   });
+
+//   xdescribe("POST /secret/message Endpoint", () => {
+//     it("Returns 'polo' when given a valid token query and posting a JSON body { key: 42, shout: 'marco' }", async () => {
+//       const res = await request
+//         .post("/secret/message")
+//         .query({ token: 2018 })
+//         .send({ key: 42, shout: "marco" });
+//       res.should.have.status(200);
+//       res.should.be.html;
+//       res.text.should.equal("polo");
+//     });
+
+//     it("Returns status 401 if token is invalid", async () => {
+//       const res = await request
+//         .post("/secret/message")
+//         .query({ token: 3 })
+//         .send({ shout: "marco" });
+//       res.should.have.status(401);
+//     });
+
+//     it("Returns status 403 if token is valid, but the secret part is missing", async () => {
+//       const res = await request.post("/secret/message").query({ token: 6 });
+//       res.should.have.status(403);
+//     });
+
+//     it("Returns status 401 if anything is wrong", async () => {
+//       const res = await request.post("/secret/message");
+//       res.should.have.status(401);
+//     });
+//   });
+// });
